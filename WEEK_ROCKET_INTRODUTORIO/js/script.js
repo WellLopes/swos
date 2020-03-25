52495e1b85ead86559666ee3a7dd50c7f9595d50
Well <well@edu.unifor.br>
2020-03-25 13:07:27 -0300

    subindo todos os arquivos

---
+++ WEEK_ROCKET_INTRODUTORIO/js/script.js
@@ -0,0 +1,6 @@
+function onOff() { // ouvidor de eventos
+    document
+        .querySelector("#modal")
+        .classList //propriedade
+        .toggle("hide") //funcao de colcar e tirar o "hide"
+}
