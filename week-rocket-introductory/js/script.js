e4ec461463baea276d0b605ddbaa295586e19607
Well <well@edu.unifor.br>
2020-03-25 20:06:23 -0300

    update

--- week-rocket-introductory/js/script.js
+++ week-rocket-introductory/js/script.js
@@ -1,6 +1,18 @@
 function onOff() { // ouvidor de eventos
+        
     document
         .querySelector("#modal")
         .classList //propriedade
-        .toggle("hide") //funcao de colcar e tirar o "hide"
+        .toggle("hide") //funcao de colocar e tirar o "hide"
+    
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+
 }
